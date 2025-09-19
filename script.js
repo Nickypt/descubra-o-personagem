f50d132f98f82b3fe8dd8a3aeefdc02001c6bf94
nicolly puerta <nicolly.puerta@escola.pr.gov.br>
2025-09-19 11:02:13 -0300

    ok

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-const personagens = [
-    { nome: "Batman", dicas: ["Ele é um bilionário que combate o crime em Gotham City.", "Não tem superpoderes, mas usa tecnologia e gadgets avançados."], imagemUrl: "https://i.imgur.com/Kx3u45L.jpeg" },
-    { nome: "Capitão América", dicas: ["Ele é um super-soldado da Segunda Guerra Mundial, que foi congelado no tempo.", "Seu principal acessório é um escudo indestrutível feito de vibranium."], imagemUrl: "https://i.imgur.com/J8Yl00H.jpeg" },
-    { nome: "Jack Sparrow", dicas: ["Ele é um pirata carismático, com um jeito de andar e falar únicos.", "Capitão do navio Pérola Negra, ele está sempre procurando tesouros e evitando a Companhia das Índias Orientais."], imagemUrl: "https://i.imgur.com/gK1kFpY.jpeg" },
-    { nome: "Darth Vader", dicas: ["É um dos vilões mais icônicos do cinema, com uma respiração pesada e robótica.", "Ele é um Lorde Sith, conhecido por ser o principal executor do Império Galáctico."], imagemUrl: "https://i.imgur.com/rS2a88U.jpeg" },
-    { nome: "Elsa", dicas: ["Ela é uma rainha que nasceu com poderes de gelo e neve.", "Sua música tema mais famosa é 'Let it Go', e seu castelo é feito de gelo."], imagemUrl: "https://i.imgur.com/39wF4y8.jpeg" },
-    { nome: "Frodo Bolseiro", dicas: ["Ele é um hobbit, uma criatura pequena que vive em um lugar chamado o Condado.", "Sua missão é destruir um anel mágico e poderoso em uma montanha de fogo."], imagemUrl: "https://i.imgur.com/QhXoB2i.jpeg" },
-    { nome: "Gandalf", dicas: ["Ele é um mago poderoso, membro da Ordem de Istari.", "Guia os hobbits, anões e humanos em uma missão para derrotar um grande mal."], imagemUrl: "https://i.imgur.com/lM3Lp8U.jpeg" },
-    { nome: "Goku", dicas: ["É um dos últimos Saiyajins, um guerreiro do Planeta Vegeta.", "Sua técnica principal é o Kamehameha, e ele pode se transformar em Super Saiyajin."], imagemUrl: "https://i.imgur.com/fVq6X0k.jpeg" },
-    { nome: "Harry Potter", dicas: ["É um bruxo que sobreviveu à maldição de Voldemort quando era um bebê.", "Ele tem uma cicatriz na testa em forma de raio."], imagemUrl: "https://i.imgur.com/kYq3Q0L.jpeg" },
-    { nome: "Homem de Ferro", dicas: ["Ele é um inventor e bilionário excêntrico.", "Usa uma armadura de alta tecnologia para combater o crime e salvar o mundo."], imagemUrl: "https://i.imgur.com/mRzR28g.jpeg" },
-    { nome: "Homem Aranha", dicas: ["Ele é um jovem fotógrafo que foi picado por um aracnídeo radioativo.", "Seus poderes incluem teias que ele atira dos pulsos e um 'sentido aranha' que o alerta sobre perigos."], imagemUrl: "https://i.imgur.com/bWkQ2oZ.jpeg" },
-    { nome: "Hulk", dicas: ["É um cientista que, após um experimento, se transforma em uma criatura gigante e verde quando fica irritado.", "Sua força é proporcional à sua raiva."], imagemUrl: "https://i.imgur.com/p51tV3s.jpeg" },
-    { nome: "Link", dicas: ["Ele é um herói da série 'The Legend of Zelda'.", "Normalmente usa uma túnica verde e carrega uma espada e um escudo, sendo o portador da Triforce da Coragem."], imagemUrl: "https://i.imgur.com/3h8Q45z.jpeg" },
-    { nome: "Luffy", dicas: ["Ele é o capitão do bando do Chapéu de Palha.", "Seu corpo se tornou de borracha após ele comer uma fruta do diabo."], imagemUrl: "https://i.imgur.com/p1zPq1L.jpeg" },
-    { nome: "Luke Skywalker", dicas: ["Ele é o herói de uma galáxia muito, muito distante.", "É um mestre Jedi, filho de um dos maiores vilões do cinema."], imagemUrl: "https://i.imgur.com/4qD4f1s.jpeg" },
-    { nome: "Mario", dicas: ["É um encanador italiano do Reino Cogumelo.", "Ele pula em inimigos e come cogumelos para crescer."], imagemUrl: "https://i.imgur.com/7gK5J5S.jpeg" },
-    { nome: "Mickey Mouse", dicas: ["É um personagem de desenho animado que usa luvas brancas e sapatos grandes.", "Ele mora em 'Mickey Mouse Clubhouse' e trabalha em 'Fantasia'."], imagemUrl: "https://i.imgur.com/jW1hG1U.jpeg"},
-    { nome: "Mulher Maravilha", dicas: ["Ela é uma princesa guerreira de Themyscira, uma ilha oculta.", "Seu principal acessório é o Laço da Verdade, que força as pessoas a dizerem a verdade."], imagemUrl: "https://i.imgur.com/z0J6t8Y.jpeg" },
-    { nome: "Optimus Prime", dicas: ["Ele é o líder dos Autobots.", "Seu corpo se transforma em um caminhão, e sua frase mais famosa é 'Autobots, roll out!'."], imagemUrl: "https://i.imgur.com/p8b6Q1c.jpeg" },
-    { nome: "Peter Pan", dicas: ["Ele é um menino que se recusa a crescer e mora em uma ilha mágica.", "Voa com a ajuda de pó de fada e é amigo de uma pequena fada chamada Sininho."], imagemUrl: "https://i.imgur.com/e2N6B4l.jpeg" },
-    { nome: "Sherlock Holmes", dicas: ["É um detetive particular britânico, conhecido por sua grande inteligência e observação.", "Vive na Rua Baker, 221B, em Londres, e é acompanhado por seu amigo Dr. Watson."], imagemUrl: "https://i.imgur.com/dK3f0bS.jpeg" },
-    { nome: "SpongeBob SquarePants", dicas: ["É uma criatura amarela que mora em um abacaxi, no fundo do mar.", "Seu melhor amigo é uma estrela do mar, e ele trabalha no Siri Cascudo."], imagemUrl: "https://i.imgur.com/q7N6y0w.jpeg" },
-    { nome: "Super-Homem", dicas: ["Ele é um alienígena do planeta Krypton, enviado à Terra ainda bebê.", "Seus poderes incluem superforça, voo e visão de raio-x."], imagemUrl: "https://i.imgur.com/qR5K5L2.jpeg" }
-];
+// Dicionário de personagens por nível de dificuldade
+const personagens = {
+    facil: [
+        { nome: "Mario", dicas: ["Ele é um encanador italiano do Reino Cogumelo.", "Ele pula em inimigos e come cogumelos para crescer."], imagemUrl: "https://i.imgur.com/7gK5J5S.jpeg" },
+        { nome: "Homem de Ferro", dicas: ["Ele é um inventor e bilionário excêntrico.", "Usa uma armadura de alta tecnologia para combater o crime e salvar o mundo."], imagemUrl: "https://i.imgur.com/mRzR28g.jpeg" },
+        { nome: "Goku", dicas: ["É um dos últimos Saiyajins, um guerreiro do Planeta Vegeta.", "Sua técnica principal é o Kamehameha, e ele pode se transformar em Super Saiyajin."], imagemUrl: "https://i.imgur.com/fVq6X0k.jpeg" },
+        { nome: "Darth Vader", dicas: ["É um dos vilões mais icônicos do cinema, com uma respiração pesada e robótica.", "Ele é um Lorde Sith, conhecido por ser o principal executor do Império Galáctico."], imagemUrl: "https://i.imgur.com/rS2a88U.jpeg" },
+        { nome: "Homem Aranha", dicas: ["Ele é um jovem fotógrafo que foi picado por um aracnídeo radioativo.", "Seus poderes incluem teias que ele atira dos pulsos e um 'sentido aranha' que o alerta sobre perigos."], imagemUrl: "https://i.imgur.com/bWkQ2oZ.jpeg" },
+        { nome: "Batman", dicas: ["Ele é um bilionário que combate o crime em Gotham City.", "Não tem superpoderes, mas usa tecnologia e gadgets avançados."], imagemUrl: "https://i.imgur.com/Kx3u45L.jpeg" },
+    ],
+    medio: [
+        { nome: "Capitão América", dicas: ["Ele é um super-soldado da Segunda Guerra Mundial, que foi congelado no tempo.", "Seu principal acessório é um escudo indestrutível feito de vibranium."], imagemUrl: "https://i.imgur.com/J8Yl00H.jpeg" },
+        { nome: "Jack Sparrow", dicas: ["Ele é um pirata carismático, com um jeito de andar e falar únicos.", "Capitão do navio Pérola Negra, ele está sempre procurando tesouros e evitando a Companhia das Índias Orientais."], imagemUrl: "https://i.imgur.com/gK1kFpY.jpeg" },
+        { nome: "Elsa", dicas: ["Ela é uma rainha que nasceu com poderes de gelo e neve.", "Sua música tema mais famosa é 'Let it Go', e seu castelo é feito de gelo."], imagemUrl: "https://i.imgur.com/39wF4y8.jpeg" },
+        { nome: "Mulher Maravilha", dicas: ["Ela é uma princesa guerreira de Themyscira, uma ilha oculta.", "Seu principal acessório é o Laço da Verdade, que força as pessoas a dizerem a verdade."], imagemUrl: "https://i.imgur.com/z0J6t8Y.jpeg" },
+        { nome: "SpongeBob SquarePants", dicas: ["É uma criatura amarela que mora em um abacaxi, no fundo do mar.", "Seu melhor amigo é uma estrela do mar, e ele trabalha no Siri Cascudo."], imagemUrl: "https://i.imgur.com/q7N6y0w.jpeg" },
+        { nome: "Optimus Prime", dicas: ["Ele é o líder dos Autobots.", "Seu corpo se transforma em um caminhão, e sua frase mais famosa é 'Autobots, roll out!'."], imagemUrl: "https://i.imgur.com/p8b6Q1c.jpeg" },
+    ],
+    dificil: [
+        { nome: "Frodo Bolseiro", dicas: ["Ele é um hobbit, uma criatura pequena que vive em um lugar chamado o Condado.", "Sua missão é destruir um anel mágico e poderoso em uma montanha de fogo."], imagemUrl: "https://i.imgur.com/QhXoB2i.jpeg" },
+        { nome: "Luffy", dicas: ["Ele é o capitão do bando do Chapéu de Palha.", "Seu corpo se tornou de borracha após ele comer uma fruta do diabo."], imagemUrl: "https://i.imgur.com/p1zPq1L.jpeg" },
+        { nome: "Link", dicas: ["Ele é um herói da série 'The Legend of Zelda'.", "Normalmente usa uma túnica verde e carrega uma espada e um escudo, sendo o portador da Triforce da Coragem."], imagemUrl: "https://i.imgur.com/3h8Q45z.jpeg" },
+        { nome: "Sherlock Holmes", dicas: ["É um detetive particular britânico, conhecido por sua grande inteligência e observação.", "Vive na Rua Baker, 221B, em Londres, e é acompanhado por seu amigo Dr. Watson."], imagemUrl: "https://i.imgur.com/dK3f0bS.jpeg" },
+        { nome: "Gandalf", dicas: ["Ele é um mago poderoso, membro da Ordem de Istari.", "Guia os hobbits, anões e humanos em uma missão para derrotar um grande mal."], imagemUrl: "https://i.imgur.com/lM3Lp8U.jpeg" },
+        { nome: "Peter Pan", dicas: ["Ele é um menino que se recusa a crescer e mora em uma ilha mágica.", "Voa com a ajuda de pó de fada e é amigo de uma pequena fada chamada Sininho."], imagemUrl: "https://i.imgur.com/e2N6B4l.jpeg" },
+    ]
+};
 
 const META_PONTOS = 30;
 const TEMPO_MAXIMO = 60;
@@ -34,15 +36,22 @@ let dicaAtual = -1;
 let nomeJogador = "";
 let tempoRestante = TEMPO_MAXIMO;
 let timer;
+let nivelDificuldade = "";
 
 // Elementos das telas
 const startScreen = document.getElementById('start-screen');
+const difficultyScreen = document.getElementById('difficulty-screen');
 const gameScreen = document.getElementById('game-screen');
 const endScreen = document.getElementById('end-screen');
 
 // Elementos da tela de início
 const startBtn = document.getElementById('start-btn');
 const nameInput = document.getElementById('name-input');
+
+// Elementos da tela de dificuldade
+const easyBtn = document.getElementById('easy-btn');
+const mediumBtn = document.getElementById('medium-btn');
+const hardBtn = document.getElementById('hard-btn');
 
 // Elementos da tela de jogo
 const inputPalpite = document.getElementById('adivinhaInput');
@@ -56,6 +65,7 @@ const pontuacaoTexto = document.getElementById('pontuacao');
 const metaTexto = document.getElementById('meta');
 const playerNameDisplay = document.getElementById('player-name-display');
 const timerDisplay = document.getElementById('timer-display');
+const dificuldadeDisplay = document.getElementById('dificuldade-display');
 
 // Elementos da tela de fim de jogo
 const endTitle = document.getElementById('end-title');
@@ -64,19 +74,20 @@ const endMessage = document.getElementById('end-message');
 const endContainer = document.getElementById('end-container');
 const playAgainBtn = document.getElementById('play-again-btn');
 
-// --- Funções de Lógica do Jogo ---
+// Efeitos Sonoros
+const somAcerto = document.getElementById('somAcerto');
+const somErro = document.getElementById('somErro');
+const somVitoria = document.getElementById('somVitoria');
 
-function iniciarNovoJogoCompleto() {
-    pontuacao = 0;
-    metaTexto.textContent = `Meta: ${META_PONTOS}`;
-    pontuacaoTexto.textContent = `Pontos: ${pontuacao}`;
-    iniciarNovaRodada();
-    iniciarCronometro();
+function playSound(sound) {
+    sound.currentTime = 0;
+    sound.play().catch(e => console.error("Erro ao tocar áudio:", e));
 }
 
 function iniciarNovaRodada() {
-    const indiceAleatorio = Math.floor(Math.random() * personagens.length);
-    personagemSecreto = personagens[indiceAleatorio];
+    const personagensDoNivel = personagens[nivelDificuldade];
+    const indiceAleatorio = Math.floor(Math.random() * personagensDoNivel.length);
+    personagemSecreto = personagensDoNivel[indiceAleatorio];
 
     tentativas = 0;
     dicaAtual = -1;
@@ -91,6 +102,7 @@ function iniciarNovaRodada() {
     personagemImagem.src = '';
     personagemImagem.classList.add('hidden');
     playerNameDisplay.textContent = `Olá, ${nomeJogador}!`;
+    dificuldadeDisplay.textContent = `Nível: ${nivelDificuldade.charAt(0).toUpperCase() + nivelDificuldade.slice(1)}`;
     inputPalpite.focus();
 }
 
@@ -115,10 +127,17 @@ function verificarPalpite() {
         atualizarPontuacao(pontosGanhos);
         mensagem.textContent = `Parabéns, ${nomeJogador}! Você acertou em ${tentativas} tentativa(s) e ganhou ${pontosGanhos} pontos!`;
         mensagem.className = 'win-message';
+        playSound(somAcerto);
         fimDeRodada();
     } else {
         mensagem.textContent = 'Incorreto. Tente novamente!';
-        mensagem.className = 'lose-message';
+        mensagem.className = 'lose-message shake';
+        playSound(somErro);
+        // Remove a classe de animação após a conclusão
+        setTimeout(() => {
+            mensagem.classList.remove('shake');
+        }, 500);
+
         if (tentativas >= 3) {
             mensagem.textContent = `Você errou 3 vezes, ${nomeJogador}. O personagem era "${personagemSecreto.nome}".`;
             mensagem.className = 'lose-message';
@@ -180,7 +199,7 @@ function fimDeJogoTotal(resultado) {
         endIcon.classList.add('fa-trophy', 'win');
         endContainer.classList.add('win');
         endMessage.textContent = `Você atingiu a meta de ${META_PONTOS} pontos e terminou com ${pontuacao} pontos.`;
-        // Cria a animação de confetes
+        playSound(somVitoria);
         for (let i = 0; i < 50; i++) {
             const confetti = document.createElement('div');
             confetti.classList.add('confetti-piece');
@@ -194,6 +213,7 @@ function fimDeJogoTotal(resultado) {
         endIcon.classList.add('fa-sad-cry', 'lose');
         endContainer.classList.add('lose');
         endMessage.textContent = `O personagem era "${personagemSecreto.nome}". Você terminou com ${pontuacao} pontos. Tente novamente!`;
+        btnReiniciar.textContent = "Reiniciar Jogo";
     }
 }
 
@@ -232,14 +252,42 @@ startBtn.addEventListener('click', () => {
         nomeJogador = "Jogador";
     }
     startScreen.classList.add('hidden');
+    difficultyScreen.classList.remove('hidden');
+});
+
+easyBtn.addEventListener('click', () => {
+    nivelDificuldade = 'facil';
+    difficultyScreen.classList.add('hidden');
     gameScreen.classList.remove('hidden');
     iniciarNovoJogoCompleto();
 });
 
+mediumBtn.addEventListener('click', () => {
+    nivelDificuldade = 'medio';
+    difficultyScreen.classList.add('hidden');
+    gameScreen.classList.remove('hidden');
+    iniciarNovoJogoCompleto();
+});
+
+hardBtn.addEventListener('click', () => {
+    nivelDificuldade = 'dificil';
+    difficultyScreen.classList.add('hidden');
+    gameScreen.classList.remove('hidden');
+    iniciarNovoJogoCompleto();
+});
+
+function iniciarNovoJogoCompleto() {
+    pontuacao = 0;
+    metaTexto.textContent = `Meta: ${META_PONTOS}`;
+    pontuacaoTexto.textContent = `Pontos: ${pontuacao}`;
+    iniciarNovaRodada();
+    iniciarCronometro();
+}
+
 btnReiniciar.addEventListener('click', () => {
     if (btnReiniciar.textContent === 'Reiniciar Jogo') {
-        startScreen.classList.remove('hidden');
-        gameScreen.classList.add('hidden');
+        endScreen.classList.add('hidden');
+        difficultyScreen.classList.remove('hidden');
     } else {
         iniciarNovaRodada();
     }
